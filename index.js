0f0424612dcd43d79bd6863608a3b8c633b98dde
DrLootman <nicolas.michlll@gmail.com>
2023-03-05 17:51:56 +0100

    AJAX Quest

---
+++ index.js
@@ -0,0 +1,35 @@
+// Function that get informations from Simpsons API
+
+function getSimpsonsQuotes() {
+
+    const url = "https://thesimpsonsquoteapi.glitch.me/quotes";
+
+    axios
+        .get(url)
+
+        .then((response) => response.data)
+
+        .then(([simpsons]) => {
+
+            const simpsonsHtml = `
+                <p>${simpsons.quote}</p>
+                <img src="${simpsons.image}" alt="simpsons character" />
+            `
+
+            document.querySelector('#the_simpsons').innerHTML = simpsonsHtml;
+        })
+
+        .catch((err) => {
+            console.error("This is an error", err)
+        })
+}
+
+getSimpsonsQuotes();
+
+// Button for generating a new quote and his image :
+
+const button = document.querySelector("#generator");
+
+button.addEventListener("click", () => {
+    getSimpsonsQuotes();
+});
